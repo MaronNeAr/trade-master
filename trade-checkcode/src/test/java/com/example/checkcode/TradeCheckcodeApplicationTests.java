f073565c0b932c446ce4540a0a07abd2201db728
MaronNeAr <97968316+MaronNeAr@users.noreply.github.com>
2024-02-10 15:49:11 +0800

    trade-master 1.0
    init...

---
+++ trade-checkcode/src/test/java/com/example/checkcode/TradeCheckcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.checkcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeCheckcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
