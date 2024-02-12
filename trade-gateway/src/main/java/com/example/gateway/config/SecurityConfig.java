c274c9f098d0b10da8212539b5b298af3986219c
MaronNeAr <97968316+MaronNeAr@users.noreply.github.com>
2024-02-12 21:49:24 +0800

    trade-master 1.2
    已配置网关跨域

--- trade-gateway/src/main/java/com/example/gateway/config/SecurityConfig.java
+++ trade-gateway/src/main/java/com/example/gateway/config/SecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+
+import java.util.Collections;
 
 /**
  * @author Mr.M
@@ -20,7 +25,20 @@ public class SecurityConfig {
     public SecurityWebFilterChain webFluxSecurityFilterChain(ServerHttpSecurity http) {
         return http.authorizeExchange()
                 .pathMatchers("/**").permitAll()
-                .anyExchange().authenticated()
+                .anyExchange().permitAll()
+                .and().cors().configurationSource(corsConfigurationSource())
                 .and().csrf().disable().build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedOrigins(Collections.singletonList("*"));
+        configuration.setAllowedMethods(Collections.singletonList("*"));
+        configuration.setAllowedHeaders(Collections.singletonList("*"));
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
